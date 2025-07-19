e8322840955465e76ffa9f37740404b86c91b39d
Estian01 <sebastvz01@gmail.com>
2025-07-18 22:31:36 -0500

    QOL+state feedback

--- myUART.ino
+++ myUART.ino
@@ -1,10 +1,6 @@
- 
 #include <ODriveUART.h>
-
 #include <Wire.h>
 #include <Ticker.h>
-//#include <SoftwareSerial.h>
-
 
 #define I2C_SDA 21
 #define I2C_SCL 22
@@ -14,16 +10,11 @@
 #define VDD_sensor_pin 19
 
 float u=0.0, u1=0,u2=0.0, u3=0.0, u4=0.0;
-float e=0.0, e1=0.0, e2=0.0, e3=0.0, roll1=0;
+float e=0.0, e1=0.0, e2=0.0, e3=0.0, e4=0.0, roll1=0.0;
 
-// Documentation for this example can be found here:
-// https://docs.odriverobotics.com/v/latest/guides/arduino-uart-guide.html
+// Documentation for this example can be found here: https://docs.odriverobotics.com/v/latest/guides/arduino-uart-guide.html
 
-
-////////////////////////////////
 // Set up serial pins to the ODrive
-////////////////////////////////
-
 // Below are some sample configurations.
 // You can comment out the default one and uncomment the one you wish to use.
 // You can of course use something different if you like
@@ -43,16 +34,11 @@ float e=0.0, e1=0.0, e2=0.0, e3=0.0, roll1=0;
 // HardwareSerial& odrive_serial = Serial1;
 // int baudrate = 115200; // Must match what you configure on the ODrive (see docs for details)
 
-// Arduino Mega or Due - Serial1
-//pin 16: RX - connect to ODrive TX
-//pin 17: TX - connect to ODrive RX
-// See https://www.arduino.cc/reference/en/language/functions/communication/serial/ for other options
+//ESP32
 HardwareSerial& odrive_serial = Serial2;
 int baudrate = 115200; // Must match what you configure on the ODrive (see docs for details)
 
-
 ODriveUART odrive(odrive_serial);
-
 
 // Uncomment the below line to use this axis definition:
    // X axis pointing forward
@@ -106,8 +92,8 @@ bool is_this_ready= false;
 #define PRINT_EULER 1   //Will print the Euler angles Roll, Pitch and Yaw
 
 #define STATUS_LED 13
-#define onofftime 0.4
-float G_Dt=0.02;    // Integration time (DCM algorithm)  We will run the integration loop at 50Hz if possible
+#define onofftime 0.5//0.4
+float G_Dt=0.01;    // Integration time (DCM algorithm)  We will run the integration loop at 50Hz if possible
 
 long timer=0;   //general purpuse timer
 long timer_old;
@@ -166,10 +152,9 @@ float Temporary_Matrix[3][3]={
   ,{
     0,0,0  }
 };
-
-
+//
 Ticker timeri;
-const unsigned long interval = 20;  // Intervalo de muestreo en milisegundos <------ min T= 4
+const unsigned long interval = 10;  // Intervalo de muestreo en milisegundos <------ min T= 4
 const unsigned long intervaltimer = interval; 
 
 unsigned long previousMillis = 0;
@@ -184,9 +169,19 @@ int sat_counter=0;
 int unsat_counter=0;
 int signu=0;
 
-#define vel_limit 10
+#define ROLL_OFFSET ToRad(-2.78)//-2.65  //Initial roll offset in deg/radians
 
+bool pasoxcero= false; //Se activa cuando la bici pasa por cero para activar el control
 
+//double K[] ={-0.014864251025630468433513797776868, -552.85212069801775669475318863988, -91.767190713265293311451387125999};
+const double K[] ={-0.21672060886125454892692232533591, -1565.9732934287246735038934275508, -328.84253877357650708290748298168};
+
+ 
+int sign(float u){
+  if (u>0){return 1;}
+  else if(u<0){return -1;}
+  else if(u==0){return 0;}
+}
 
 void timerCallback(){
   t=t+intervaltimer;
@@ -195,13 +190,12 @@ void timerCallback(){
   //tiempo para la referencia 
   tref=tref+intervaltimer;
   trefsegundos=tref/1000.0;
-
     
   currentMillis=t;
 
   ODriveFeedback feedback = odrive.getFeedback();
 
-if((millis()-timer)>=20)  // Main loop runs at 50Hz
+if((millis()-timer)>=10)  // Main loop runs at 50Hz
   {
     digitalWrite(STATUS_LED,HIGH);
     counter++;
@@ -211,12 +205,10 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
     {
       G_Dt = (timer-timer_old)/1000.0;    // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
       if (G_Dt > 0.2)
-        G_Dt = 0; // ignore integration times over 200 ms
+        G_Dt = 0; // ignore integration times over 100 ms   //200 ms
     }
     else
       G_Dt = 0;
-
-
 
     // * DCM algorithm
     // Data adquisition
@@ -235,35 +227,27 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
     Normalize();
     Drift_correction();
     Euler_angles();
-    // *
-
-    /*SerialBT.println(" ");
-    SerialBT.print(ToDeg(roll));
-    SerialBT.print(",");
-    SerialBT.print(ToDeg(pitch));
-    SerialBT.print(",");
-    SerialBT.print(ToDeg(yaw));
-    //Serial.print(",");*/
+     
     digitalWrite(STATUS_LED,LOW);
   }
 
-
   e=0.0-roll;
 
-
-
+  //u=-(-25*e-10*(e-e1)/interval);//10   P
+ // u=43.503246932384755041*e1 - 21.95437663765436298*e - 21.512803502947924983*e2 + 1.8535052807369114536*u1 - 0.83265300708364831106*u2;
   
-  //u=  1980947204933732608.0*e1 - 5945122943553126400.0*e2 + 5945085167421375488.0*e3 - 1980909472964295680.0*e + 3.0845805141489108792*u1 - 17993024112573.84375*u2 + 114249980.71484375*u3 + 17992909862591.042969*u4;
-  //u=19770076417658.339844*e1 - 19769950795863.484375*e2 + 6587371199687.8388672*e3 - 6587496674624.1123047*ex + 3.8801557573187892558*u1 + 59834494.844342850149*u2 - 376.27573272585868835*u3 - 59834121.44876588136*u4
-  //u= 122574473786455.1875*e1 - 122573694931330.39062*e2 + 40841701437058.320312*e3 - 40842479381659.914062*e + 3.9391765155723987846*u1 + 370973897.22023648024*u2 - 2351.7434336543083191*u3 - 370971548.41597932577*u4; //10s
-  //u=532.78227731249057797*e1 - 532.71847137722318166*e2 + 176.45038267686780387*e3 - 176.51393710318467356*e + 3.7465061096736462787*u1 - 3.8136504525674346233*u2 + 3.3252825136310186771*u3 - 2.2581381707372303325*u4;
-  //u=16.83563840393079758*e2 - 16.814363885180771518*e1 - 5.5838262804970888453*e3 + 5.5625437276747273785*e + 3.9573704705608179211*u1 - 5.9382865386571532795*u2 + 3.9107116676277757783*u3 - 0.92979559953143997575*u4;
-  //u=391658555450.66162109*e1 - 391625498822.68902588*e2 + 123495525168.18115234*e3 - 123523755944.00775146*e + 2.751173219597149*u1 + 9386123729.2959747314*u2 - 723091.94561767578125*u3 - 9385400639.1015300751*u4;
-  //u=-(10521.542487423201237*e2 - 10521.635581466498479*e1 - 3495.7164383019717206*e3 + 3495.8069072069774847*e + 3.604633562372328015*u1 - 13.8630165008556272*u2 + 2.9537982016209145542*u3 + 8.3045847368623846307*u4);
-  
-  //P
-  u=-10*e;//25
 
+u=-(K[0]*feedback.vel*2*3.14159265358979323846264 +K[1]*roll+K[2]*(roll-roll1)/interval);
+ 
+  
+  if(!pasoxcero){//u definition MUST BE BEFORE this conditional
+    //if(sign(roll)!=sign(roll1))
+    if(roll>ToRad(0.05))
+      {pasoxcero=true;}
+    u=0;
+  }
+ 
+  //u=0.06875;//0.0765625;
   if (feedback.vel>=7.6||feedback.vel<=-7.6){
     sat_counter++;
   }else{
@@ -272,14 +256,8 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
   if (sat_counter>=5){
     unsat_counter=10;
   }
-
-  
-
-
-  //u=-(45*e-20*(e-e1));
-
   //lyapunov
-  //u=((26*0.8*9.81*sin(roll)-5*(roll-roll1)/0.2-100*abs((e-e1)/0.2+e)))/16.64;
+  //u=((26.6*0.365*9.81*sin(roll)-5*(roll-roll1)/0.2-100*abs((e-e1)/0.2+e)))/16.64;
   
   
   /*if(roll>0){u=0.9;}
@@ -287,23 +265,23 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
   else if(roll==0){u=0;}*/
   /*
  if(trefsegundos>=0 && trefsegundos<onofftime){
-    u=0.8; 
+    u=0; 
     }
     if(trefsegundos>=onofftime && trefsegundos<2*onofftime){
-    u=0; 
+    u=0.9; 
     }
     if(trefsegundos>=2*onofftime && trefsegundos<3*onofftime){
-    u=-0.8;
+    u=0;
     }
     if(trefsegundos>=3*onofftime && trefsegundos<4*onofftime){
-    u=0; 
+    u=-0.9; 
     }
     if(trefsegundos>=4*onofftime){
     tref=0;
     }
-
-  */
-
+*/
+  
+/*
   if (unsat_counter>0){
     if(u>0){signu=1;}
     else if (u<0){signu=-1;}
@@ -311,21 +289,22 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
     u=-signu*1;
     unsat_counter--;
   }
+  */
 
   if(u>1.2){u=1.2;}if(u<-1.2){u=-1.2;}//limite para que no se desborde u
-  odrive.setTorque(u);
+  odrive.setTorque(u);//-u
   //odrive.setTorque(roll*1.6/0.5235987756);
   
-  //Serial.print("pos:");
-  //Serial.print(feedback.pos);
+  //Serial.print(sign(pasoxcero));
   //Serial.print(", ");
-  //Serial.print("vel:");
   printdata();
   Serial.print(u);
   Serial.print(", ");
   
 
   if (odrive.getState() == AXIS_STATE_CLOSED_LOOP_CONTROL) {
+    //Serial.print(feedback.pos);
+    //Serial.print(", ");
     Serial.print(feedback.vel);
     Serial.print(", ");
     Serial.print(odrive.getParameterAsFloat("axis0.controller.effective_torque_setpoint"));
@@ -335,16 +314,17 @@ if((millis()-timer)>=20)  // Main loop runs at 50Hz
   }
   //u=odrive.getParameterAsFloat("axis0.controller.effective_torque_setpoint");
   Serial.println();
-
   
-  u4=u3;
-  u3=u2;
-  u2=u1;
-  u1=u;
-
-  e3=e2;
-  e2=e1;
-  e1=e;
+  if(pasoxcero){
+    u4=u3;
+    u3=u2;
+    u2=u1;
+    u1=u;
+    e4=e3;
+    e3=e2;
+    e2=e1;
+    e1=e;
+  }
   roll1=roll;
 }
 
@@ -385,7 +365,8 @@ void setup() {
   for(int y=0; y<6; y++)
     AN_OFFSET[y] = AN_OFFSET[y]/32;
 
-  AN_OFFSET[5]-=GRAVITY*SENSOR_SIGN[5];
+  AN_OFFSET[5]-=GRAVITY*SENSOR_SIGN[5]*cos(ROLL_OFFSET);
+  AN_OFFSET[4]-=GRAVITY*SENSOR_SIGN[5]*sin(ROLL_OFFSET);
 
   //Serial.println("Offset:");
   for(int y=0; y<6; y++)
@@ -397,7 +378,6 @@ void setup() {
   timer=millis();
   delay(20);
   counter=0;
-
 
   odrive_serial.begin(baudrate,SERIAL_8N1,16,17);
 
@@ -411,25 +391,21 @@ void loop() {
   if (is_this_ready==false){
       while (odrive.getState() == AXIS_STATE_UNDEFINED) {
       delay(100);
+      //Serial.println("found ODrive");
+    //Serial.print("DC voltage: ");
+    //Serial.println(odrive.getParameterAsFloat("vbus_voltage"));
+
+    is_this_ready=true;
+
     }
 
-    Serial.println("found ODrive");
-    
-    Serial.print("DC voltage: ");
-    Serial.println(odrive.getParameterAsFloat("vbus_voltage"));
-    
-    Serial.println("Enabling closed loop control...");
     while (odrive.getState() != AXIS_STATE_CLOSED_LOOP_CONTROL) {
       odrive.clearErrors();
       odrive.setState(AXIS_STATE_CLOSED_LOOP_CONTROL);
       delay(10);
     }
     
-    Serial.println("ODrive running!");
-
+    //Serial.println("Enabling closed loop control...");    
+    //Serial.println("ODrive running!");
   }
-  
 }
-
-
-
